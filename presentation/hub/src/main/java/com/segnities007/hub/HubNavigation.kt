961fea5d69fc0dc02a7d8730f466e0f29a7a0c94
seg <segnities007@gmail.com>
2025-08-12 03:02:56 +0900

    add some alias(libs.plugins.kotlin.compose) in gradle.kts

--- presentation/hub/src/main/java/com/segnities007/hub/HubNavigation.kt
+++ presentation/hub/src/main/java/com/segnities007/hub/HubNavigation.kt
@@ -1,2 +1,48 @@
 package com.segnities007.hub
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.segnities007.navigation.HubRoute
+import com.segnities007.navigation.Route
+
+@Composable
+fun HubNavigation(
+    onNavigate: (Route) -> Unit,
+){
+    val hubNavController = rememberNavController()
+
+    HubUi{
+        NavHost(
+            navController = hubNavController,
+            startDestination = HubRoute.Home,
+        ){
+            composable<HubRoute.Home>{
+                //TODO
+            }
+            composable<HubRoute.Items>{
+                //TODO
+            }
+            composable<HubRoute.Dashboard>{
+                //TODO
+            }
+            composable<HubRoute.Templates>{
+                //TODO
+            }
+            composable<HubRoute.Setting>{
+                //TODO
+            }
+        }
+    }
+}
+
+@Composable
+private fun HubUi(
+    content: @Composable () -> Unit,
+){
+    Scaffold{ it
+        content()
+    }
+}
