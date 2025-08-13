838fa1518ac67f31f60b181871d730c57f0af644
seg <segnities007@gmail.com>
2025-08-14 02:57:07 +0900

    implement loginscreen

--- presentation/login/build.gradle.kts
+++ presentation/login/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     // koin
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.compose)
 }
--- presentation/login/src/main/java/com/segnities007/login/LoginScreen.kt
+++ presentation/login/src/main/java/com/segnities007/login/LoginScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -14,16 +17,36 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.segnities007.login.component.LoginButton
+import com.segnities007.login.mvi.LoginEffect
+import com.segnities007.login.mvi.LoginIntent
+import com.segnities007.login.mvi.LoginViewModel
+import org.koin.compose.koinInject
 
 @Composable
 fun LoginScreen(){
 
-    LoginUi()
+    val loginViewModel: LoginViewModel = koinInject()
+    val state by loginViewModel.state.collectAsState()
+
+    LaunchedEffect(Unit){
+        loginViewModel.effect.collect{ effect ->
+            when(effect){
+                LoginEffect.NavigateToHub -> TODO()
+                is LoginEffect.ShowToast -> TODO()
+            }
+        }
+    }
+
+
+    LoginUi(sendIntent = loginViewModel::sendIntent)
 }
 
 @Composable
-private fun LoginUi(){
+private fun LoginUi(
+    sendIntent: (LoginIntent) -> Unit,
+){
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -33,7 +56,7 @@ private fun LoginUi(){
         Spacer(modifier = Modifier.weight(1f))
         Titles()
         Spacer(modifier = Modifier.weight(1f))
-        Buttons()
+        Buttons(sendIntent)
         Spacer(modifier = Modifier.weight(0.5f))
     }
 }
@@ -54,7 +77,9 @@ private fun Titles(){
 }
 
 @Composable
-private fun Buttons(){
+private fun Buttons(
+    sendIntent: (LoginIntent) -> Unit,
+){
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -64,13 +89,13 @@ private fun Buttons(){
             text = "Continue With Google",
             res = R.drawable.icons8_google,
         ) {
-            //TODO
+            sendIntent(LoginIntent.ContinueWithGoogle)
         }
         LoginButton(
             modifier = Modifier.fillMaxWidth(),
             text = "Continue with nothing",
         ) {
-            //TODO
+            sendIntent(LoginIntent.ContinueWithNothing)
         }
     }
 }
@@ -78,5 +103,7 @@ private fun Buttons(){
 @Composable
 @Preview
 private fun LoginUiPreview(){
-    LoginUi()
+    LoginUi(
+        sendIntent = {}
+    )
 }
