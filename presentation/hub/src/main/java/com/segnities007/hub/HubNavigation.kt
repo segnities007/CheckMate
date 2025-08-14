bf19a0ba7eb8e3caa4792d25b75999dad524fb6f
seg <segnities007@gmail.com>
2025-08-14 21:13:15 +0900

    modify HubNavigation

--- presentation/hub/src/main/java/com/segnities007/hub/HubNavigation.kt
+++ presentation/hub/src/main/java/com/segnities007/hub/HubNavigation.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -108,10 +110,13 @@ private fun HubUi(
         bottomBar = bottomBar
     ){
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ){
             Spacer(modifier = Modifier.padding(top = it.calculateTopPadding()))
             content()
+            Spacer(modifier = Modifier.padding(bottom = it.calculateBottomPadding()))
         }
     }
 }
