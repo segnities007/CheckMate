b924780464abae6a785170689f2cef824aa7d022
seg <segnities007@gmail.com>
2025-10-17 22:38:15 +0900

    refactoring

--- presentation/setting/src/main/java/com/segnities007/setting/component/DataButtons.kt
+++ presentation/setting/src/main/java/com/segnities007/setting/component/DataButtons.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Upload
