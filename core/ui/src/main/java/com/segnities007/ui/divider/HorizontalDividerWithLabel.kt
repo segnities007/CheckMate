c13401743a0b49cfe5dabddef22019ed7a8d0ccc
seg <segnities007@gmail.com>
2025-10-17 22:37:40 +0900

    modify typo

--- core/ui/src/main/java/com/segnities007/ui/divider/HorizontalDividerWithLabel.kt
+++ core/ui/src/main/java/com/segnities007/ui/divider/HorizontalDividerWithLabel.kt
@@ -1,23 +1,15 @@
 package com.segnities007.ui.divider
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun HorizontalDividerWithLabel(label: String) {
